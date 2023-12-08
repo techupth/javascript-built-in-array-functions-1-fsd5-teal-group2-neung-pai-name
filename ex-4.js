605dc08920c9edc8bac1676e0b1a114bd1e85910
Neung0511 <neung@gmail.com>
2023-12-08 13:03:17 +0700

    feat(ex1-ex5):completed

--- ex-4.js
+++ ex-4.js
@@ -7,10 +7,11 @@ const todos = [
 ];
 
 function getCompletedTodo(todos) {
-  // Start coding here
+  return todos.filter((todos) => todos.completed === true);
 }
 
-getCompletedTodo(todos);
+const result = getCompletedTodo(todos);
+console.log(result);
 
 /* 
 	Output:
